d5b7c1aaa897d5456f434e392954117f7880a2f3
ARUN  KUMAR <1256175@01hw872525.local>
2018-01-18 12:44:29 +0530

    Initial Commit

---
+++ CICD_POC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CICD_POC
+//
+//  Created by ARUN  KUMAR on 18/01/18.
+//  Copyright © 2018 ARUN  KUMAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
