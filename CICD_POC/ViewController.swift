b802408871e727fff3a3fa35ac6529e0ca9ff45e
ARUN  KUMAR <1256175@01hw872525.local>
2018-01-19 12:55:24 +0530

    View method added.

--- CICD_POC/ViewController.swift
+++ CICD_POC/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
     }
 
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
 }
 
